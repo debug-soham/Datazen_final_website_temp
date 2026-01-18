d05ef1f646b725d1623103194de59ef04060cbc6
Ishika Bhoyar <145825801+ishikabhoyar@users.noreply.github.com>
2026-01-19 00:23:32 +0530

    fix: remove unused Register route from App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import { Route, Switch } from "wouter";
 import Home from "@/pages/Home";
 import Resources from "@/pages/Resources";
-import Register from "@/pages/Register";
 import NotFound from "@/pages/not-found";
 import PixelCardExample from "./components/PixelCardExample";
 import { ThemeProvider } from "./contexts/theme-context";
@@ -12,7 +11,6 @@ function App() {
       <Switch>
         <Route path="/" component={Home} />
         <Route path="/resources" component={Resources} />
-        <Route path="/register" component={Register} />
         <Route path="/pixel-cards" component={PixelCardExample} />
         <Route component={NotFound} />
       </Switch>
